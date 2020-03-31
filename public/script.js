d1142f65c59642257f9b848d767d54d29eb53f40
theandersonn <theandersonn@gmail.com>
2020-03-31 15:40:33 -0300

    validação de campos

--- public/script.js
+++ public/script.js
@@ -12,5 +12,29 @@ function onOff() {
   document
     .querySelector('#modal')
     .classList
-    .toggle('addScroll')
+    .toggle('addScroll')  
+}
+
+function checkFields(event) {
+  const valuesToCheck = [
+    "title",
+    "category",
+    "image",
+    "description",
+    "link"
+  ]
+
+  const isEmpty = valuesToCheck.find(function(value) {
+    const checkIfIsString = typeof event.target[value].value === "string"
+    const checkIfIsEmpty = !event.target[value].value.trim()
+    
+    if (checkIfIsString && checkIfIsEmpty) {
+      return true
+    }
+  })
+
+  if (isEmpty) {
+    event.preventDefault()
+    alert("Por favor preencha todo os campos!")
+  }
 }
--- server.js
+++ server.js
@@ -4,56 +4,6 @@ const server = express()
 
 const db = require("./db")
 
-// const ideas = [
-//   {
-//     img: "https://image.flaticon.com/icons/svg/2729/2729007.svg",
-//     title: "Cursos de Programação",
-//     category: "Estudo",
-//     description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem ipsum quod ut, voluptatum mollitia sequi",
-//     url: "https://github.com/theandersonn"
-//   },
-
-//   {
-//     img: "https://image.flaticon.com/icons/svg/2729/2729005.svg",
-//     title: "Exercícios",
-//     category: "Saúde",
-//     description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem ipsum quod ut, voluptatum mollitia sequi",
-//     url: "https://github.com/theandersonn"
-//   },
-  
-//   {
-//     img: "https://image.flaticon.com/icons/svg/2729/2729027.svg",
-//     title: "Meditação",
-//     category: "Mentalidade",
-//     description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem ipsum quod ut, voluptatum mollitia sequi",
-//     url: "https://github.com/theandersonn"
-//   },
-
-//   {
-//     img: "https://image.flaticon.com/icons/svg/2729/2729032.svg",
-//     title: "Karaokê",
-//     category: "Estudo",
-//     description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem ipsum quod ut, voluptatum mollitia sequi",
-//     url: "https://github.com/theandersonn"
-//   },
-  
-//   {
-//     img: "https://image.flaticon.com/icons/svg/2729/2729038.svg",
-//     title: "Pintura",
-//     category: "Criatividade",
-//     description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem ipsum quod ut, voluptatum mollitia sequi",
-//     url: "https://github.com/theandersonn"
-//   },
-
-//   {
-//     img: "https://image.flaticon.com/icons/svg/2729/2729048.svg",
-//     title: "Recortes",
-//     category: "Criatividade",
-//     description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem ipsum quod ut, voluptatum mollitia sequi",
-//     url: "https://github.com/theandersonn"
-//   }   
-// ]
-
 // configurar arquivos estáticos (css, scripts, images)
 server.use(express.static("public"))
 
